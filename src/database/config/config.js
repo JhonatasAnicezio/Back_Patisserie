295f3b7d78851166117025580d649e8a8bb2da17
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-04 14:38:16 -0300

    feat: fix config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,10 @@ require('dotenv/config');
 
 const config = {
 	username: process.env.MYSQLUSER || 'root',
-	password: process.env.MYSQLPASSWORD || 'senha-mysql',
-	database: process.env.MYSQLDATABASE || 'DB_USER_PATISSERIE',
-	host: process.env.MYSQLHOST || 'localhost',
-	port: process.env.MYSQLPORT  || 3306,
+  password: process.env.MYSQLPASSWORD || 'senha-mysql',
+  database: process.env.MYSQLDATABASE || 'DB_USER_PATISSERIE',
+  host: process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || 3306,
 	dialect: 'mysql',
 	dialectOptions: {
 		timezone: 'Z',
