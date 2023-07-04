a7172f172ceec0f7d9770a717c35182579637793
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-04 11:30:03 -0300

    feat: alter envi

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,11 +1,11 @@
 require('dotenv/config');
 
 const config = {
-	username: process.env.DB_USER || 'root',
-	password: process.env.DB_PASS || 'senha-mysql',
-	database: 'DB_USER_MANAGEMENT',
-	host: process.env.DB_HOST || 'localhost',
-	port: Number(process.env.DB_PORT) || 3306,
+	username: process.env.MYSQLUSER || 'root',
+	password: process.env.MYSQLPASSWORD || 'senha-mysql',
+	database: 'DB_USER_PATISSERIE',
+	host: process.env.MYSQLHOST || 'localhost',
+	port: Number(process.env.MYSQLPORT) || 3306,
 	dialect: 'mysql',
 	dialectOptions: {
 		timezone: 'Z',
