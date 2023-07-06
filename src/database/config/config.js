8c60a04e2217a318f8941ffbf1821539c8662d11
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-06 11:00:15 -0300

    feat: create new config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,12 +1,12 @@
 require('dotenv/config');
 
 const config = {
-	username: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'senha-mysql',
-  database: process.env.MYSQLDATABASE || 'DB_USER_PATISSERIE',
-  host: process.env.MYSQLHOST || 'localhost',
-  port: process.env.MYSQLPORT || 3306,
-	dialect: 'mysql',
+	username: process.env.POSTGRES_USER || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'minha-senha',
+  database: process.env.POSTGRES_DATABASE || 'DB_USER_PATISSERIE',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: process.env.POSTGRES_PORT || 5432,
+	dialect: 'postgres',
 };
 
 module.exports = config;
