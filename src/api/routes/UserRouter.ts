82101e7fac7490b0d75cdcb1caec7f70ebd4aa08
Jhonatas Anicezio <jhonatasanic@hotmail.com>
2023-07-07 16:43:12 -0300

    refact

--- src/api/routes/UserRouter.ts
+++ src/api/routes/UserRouter.ts
@@ -4,53 +4,46 @@ import UserService from '../services/UserService';
 import validateFieldsUser from '../middlewares/validateFieldsUser';
 import authToken from '../middlewares/authToken';
 
-const teamRouter = Router();
+const userRouter = Router();
 
 const userService = new UserService();
 const userController = new UserController(userService);
 
-teamRouter.get(
+userRouter.get(
   '/me',
   authToken.verify,
   (req: Request, res: Response) => userController.getUser(req, res),
 );
 
-teamRouter.get(
+userRouter.get(
   '/',
   (req: Request, res: Response) => userController.getUsers(req, res),
 );
 
-teamRouter.post(
+userRouter.post(
   '/login',
   validateFieldsUser.verify,
   (req: Request, res: Response) => userController.postLogin(req, res),
 );
 
-teamRouter.post(
+userRouter.post(
   '/',
   validateFieldsUser.verifyRegister,
   (req: Request, res: Response) => userController.postRegisterUser(req, res),
 );
 
-teamRouter.delete(
+userRouter.delete(
   '/:id',
   authToken.verify,
   authToken.verifyAdmin,
   (req: Request, res: Response) => userController.deleteUser(req, res),
 );
 
-teamRouter.get(
-  '/',
-  authToken.verify,
-  authToken.verifyAdmin,
-  (req: Request, res: Response) => userController.getUsers(req, res),
-);
-
-teamRouter.put(
+userRouter.put(
   '/:id',
   authToken.verify,
   authToken.verifyAdmin,
   (req: Request, res: Response) => userController.updateUsers(req, res),
 );
 
-export default teamRouter;
+export default userRouter;
